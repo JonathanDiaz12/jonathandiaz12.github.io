43aeac8a17fd9b2c9482906fb2eca714ee265d9e
JonathanDiaz12 <diaz.j023958@stu.sanjac.edu>
2025-05-07 04:58:31 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+/* Created in CSS HTML Validator for Windows */
+
+document.getElementById("donateButton").onclick = function (){
+    window.open("https://hcb.hackclub.com/donations/start/circuit-sorcerers","_blank");
+};
+document.getElementById('downloadLink').href = 'js/CircuitSorcerersPacket.pdf';
